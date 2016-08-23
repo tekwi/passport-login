ad56663e7a557a733997fee77978f939fd108af9
Sam Titus <sam.titus1@hp.com>
2016-08-23 10:08:37 -0700

    debugging Oauth to AVS

--- src/routes/login.js
+++ src/routes/login.js
@@ -28,8 +28,8 @@ module.exports = function(passport){
  
   /* Handle Registration POST */
   router.post('/register', passport.authenticate('local-signup', {
-    successRedirect: 'https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.4f40bfef768c4087ab37f25ead297561&scope=alexa%3Aall&scope_data=%7B%22alexa%3Aall%22%3A%7B%22productID%22%3A%22macedone%22,%22productInstanceAttributes%22%3A%7B%22deviceSerialNumber%22%3A%20%22TH65A2Y124%22%7D%7D%7D&response_type=code&redirect_uri=https%3A%2F%2Ftest.printstacks.com',
-    //successRedirect: 'https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.34e3102618564b9c93931ab6751b899a&scope=alexa%3Aall&scope_data=%7B%22alexa%3Aall%22%3A%7B%22productID%22%3A%22TEKWI_VOICE%22,%22productInstanceAttributes%22%3A%7B%22deviceSerialNumber%22%3A%20%22TH65A2Y124%22%7D%7D%7D&response_type=code&redirect_uri=https%3A%2F%2Ftest.printstacks.com',
+    // successRedirect: 'https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.4f40bfef768c4087ab37f25ead297561&scope=alexa%3Aall&scope_data=%7B%22alexa%3Aall%22%3A%7B%22productID%22%3A%22macedone%22,%22productInstanceAttributes%22%3A%7B%22deviceSerialNumber%22%3A%20%22TH65A2Y124%22%7D%7D%7D&response_type=code&redirect_uri=https%3A%2F%2Ftest.printstacks.com',
+    successRedirect: 'https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.34e3102618564b9c93931ab6751b899a&scope=alexa%3Aall&scope_data=%7B%22alexa%3Aall%22%3A%7B%22productID%22%3A%22TEKWI_VOICE%22,%22productInstanceAttributes%22%3A%7B%22deviceSerialNumber%22%3A%20%22TH65A2Y124%22%7D%7D%7D&response_type=code&redirect_uri=https%3A%2F%2Ftest.printstacks.com',
     failureRedirect: '/register',
     failureFlash : true 
   }));

d74979022c6266a3f9fff9e79940b6feea5cc303
Sam Titus <sam.titus1@hp.com>
2016-08-26 16:55:59 -0700

    adding alexa outh url

--- src/routes/home.js
+++ src/routes/home.js
@@ -33,7 +33,7 @@ router.get('/', isAuthenticated, function(req, res){
  
 // /* GET home page. */
 router.get('/', verifyAuthencticated, function(req, res, next) {
-  res.render('home', { message: req.flash("message"), warning: req.flash("warning"), alexa_oauth : "https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.4f40bfef768c4087ab37f25ead297561&scope=alexa%3Aall&scope_data=%7B%22alexa%3Aall%22%3A%7B%22productID%22%3A%22macedon%22,%22productInstanceAttributes%22%3A%7B%22deviceSerialNumber%22%3A%20%22TH65A2Y124%22%7D%7D%7D&response_type=code&redirect_uri=https%3A%2F%2Ftest.printstacks.com/auth" });
+  res.render('home', { message: req.flash("message"), warning: req.flash("warning"), alexa_oauth : "https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.4f40bfef768c4087ab37f25ead297561&scope=alexa%3Aall&scope_data=%7B%22alexa%3Aall%22%3A%7B%22productID%22%3A%22macedon%22,%22productInstanceAttributes%22%3A%7B%22deviceSerialNumber%22%3A%20%22TH65A2Y124%22%7D%7D%7D&response_type=code&redirect_uri=https%3A%2F%2F"+ req.headers.host +"/auth" });
 
 });
 module.exports = router;

130192ce76eeb48e6675493777c7a56aebdaac7f
Sam Titus <sam.titus1@hp.com>
2016-08-26 15:03:48 -0700

    adding prod url hardcoded

--- src/routes/login.js
+++ src/routes/login.js
@@ -5,14 +5,71 @@ module.exports = function(passport){
 
  /* GET login page. */
   router.get('/', function(req, res) {
-    console.log("https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.4f40bfef768c4087ab37f25ead297561&scope=alexa%3Aall&scope_data=%7B%22alexa%3Aall%22%3A%7B%22productID%22%3A%22macedon%22,%22productInstanceAttributes%22%3A%7B%22deviceSerialNumber%22%3A%20%22TH65A2Y124%22%7D%7D%7D&response_type=code&redirect_uri="+req.headers.host+"/auth");
-    res.render('index', { message: req.flash('info') });
+      res.render('index', { message: req.flash('message') });
   });
     
   router.get('/login', function(req, res) {
       res.render('index', { message: req.flash('message') });
   });
   
+/* GET login page. */
+  // router.get('/:code?/:scope?', function(req, res) {
+  //           res.render('index', { message: req.flash('info') });
+  // })
+    // if (req.query.code && req.query.scope) { 
+    //   //
+    //   var uuid = require('node-uuid');
+
+
+    //     var AWS = require("aws-sdk")
+        
+    //     AWS.config.update({
+    //       region: "us-west-2",
+    //     });
+        
+    //     var docClient = new AWS.DynamoDB.DocumentClient();
+    //     var table = "authcodes";
+    //     var params = {
+    //       TableName:table,
+    //       Item:{
+    //         "code":req.session.code,
+    //         "timestamp":new Date().getTime(),
+    //         "id":uuid.v4(),
+    //         "user": "test" 
+    //       } 
+    //     };  
+    //     if (req.session.code) { 
+    //       docClient.put(params, function(err, data) {
+    //         if (err) {
+    //           res.send("error: " + JSON.stringify(err, null, 2));
+    //         } else {
+    //           //req.session.destroy(function(err){});
+    //           res.send("Congratulations, you are now registered and ready to use voice-enabled printer.");
+    //         }
+    //       });
+    //     }else{
+    //       res.send("Authcode not valid.");
+    //     }
+    //       // res.send("Success! You are now ready to talk to MACEDON!");
+    //     }else
+  //           res.render('index', { message: req.flash('info') });
+  // });
+  /* GET login page. */
+  // router.get('/:code?/:scope?', function(req, res) {
+  //   if (req.query.code && req.query.scope) {
+  //     /* GET users listing. */
+  //      req.session.code = req.query.code;
+  //      req.session.scope = req.query.scope;
+
+  //      res.redirect("/auth");
+       
+  //     //res.send("Success! You are now ready to talk to MACEDON!");
+  //   }else
+  //     res.render('index', { message: req.flash('info') });
+    
+  // });
+ 
+
   /* Handle Login POST */
   router.post('/login', passport.authenticate('local-login', {
     successRedirect: '/home',
@@ -38,7 +95,7 @@ module.exports = function(passport){
  
   /* Handle Registration POST */
   router.post('/register', passport.authenticate('local-signup', {
-    https: "successRedirect://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.4f40bfef768c4087ab37f25ead297561&scope=alexa%3Aall&scope_data=%7B%22alexa%3Aall%22%3A%7B%22productID%22%3A%22macedon%22,%22productInstanceAttributes%22%3A%7B%22deviceSerialNumber%22%3A%20%22TH65A2Y124%22%7D%7D%7D&response_type=code&redirect_uri=https%3A%2F%2F"+"www.printstacks.com"+"/auth",
+    successRedirect: 'https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.4f40bfef768c4087ab37f25ead297561&scope=alexa%3Aall&scope_data=%7B%22alexa%3Aall%22%3A%7B%22productID%22%3A%22macedon%22,%22productInstanceAttributes%22%3A%7B%22deviceSerialNumber%22%3A%20%22TH65A2Y124%22%7D%7D%7D&response_type=code&redirect_uri=https%3A%2F%2Fwww.printstacks.com/auth',
  // successRedirect: 'https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.34e3102618564b9c93931ab6751b899a&scope=alexa%3Aall&scope_data=%7B%22alexa%3Aall%22%3A%7B%22productID%22%3A%22TEKWI_VOICE%22,%22productInstanceAttributes%22%3A%7B%22deviceSerialNumber%22%3A%20%22TH65A2Y124%22%7D%7D%7D&response_type=code&redirect_uri=https%3A%2F%2Ftest.printstacks.com',
     failureRedirect: '/register',
     failureFlash : true 

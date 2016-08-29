f6023a57c9596ab5571283aafd4f556f14a72577
Sam Titus <sam.titus1@hp.com>
2016-08-29 14:46:48 -0700

    adding acl to separate file

---
+++ src/lib/acl.js
@@ -0,0 +1,10 @@
+
+module.exports = function(req,res,next) {
+		if (req.isAuthenticated()){
+			req.flash("message", "Hi "+ req.user.firstName.S +", Welcome to Macedon!");
+			next(null, {"success": "1"});
+		} else {
+			req.flash("message", "You need to login first!");
+			res.redirect("/login?rurl="+req.baseUrl);
+		}
+}

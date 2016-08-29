2909f725d76897a241b27dcd6fd569113aec7baf
Sam Titus <sam.titus1@hp.com>
2016-08-29 11:30:55 -0700

    alexa link to home page

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -28,12 +28,12 @@ router.get('/:code?/:scope?', function(req, res, next) {
       } else {
         //req.session.destroy(function(err){});
         req.flash("message", "Congratulations "+ req.user.firstName.S +", you are now registered and ready to use voice-enabled printer.");
-        res.render("home", {message : req.flash("message"), warning: req.flash("warning")});
+        res.render("home", {message : req.flash("message"), warning: req.flash("warning"), alexa_oauth : "https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.4f40bfef768c4087ab37f25ead297561&scope=alexa%3Aall&scope_data=%7B%22alexa%3Aall%22%3A%7B%22productID%22%3A%22macedon%22,%22productInstanceAttributes%22%3A%7B%22deviceSerialNumber%22%3A%20%22TH65A2Y124%22%7D%7D%7D&response_type=code&redirect_uri=https%3A%2F%2F"+ req.headers.host +"/auth"});
       }
     });
   }else{
       req.flash("warning", "Authorization failed. Please try again!");
-      res.render("home", {message : req.flash("message"), warning: req.flash("warning")});
+      res.render("home", {message : req.flash("message"), warning: req.flash("warning"), alexa_oauth : "https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.4f40bfef768c4087ab37f25ead297561&scope=alexa%3Aall&scope_data=%7B%22alexa%3Aall%22%3A%7B%22productID%22%3A%22macedon%22,%22productInstanceAttributes%22%3A%7B%22deviceSerialNumber%22%3A%20%22TH65A2Y124%22%7D%7D%7D&response_type=code&redirect_uri=https%3A%2F%2F"+ req.headers.host +"/auth"});
       //res.send("Authcode not valid.");
   }
     // res.send("Success! You are now ready to talk to MACEDON!");

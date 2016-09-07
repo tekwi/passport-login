ece838c546989957537f963ac918d97d587be070
Sam Titus <sam.titus1@hp.com>
2016-09-07 13:46:41 -0700

    removed unused view files

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var verifier = require('alexa-verifier');
 
-//var routes = require('./routes/index');
+
 var homepage = require('./routes/home');
 var faq = require('./routes/faq');
 var users = require('./routes/users');
@@ -37,10 +37,8 @@ DynamoDBStore = require('connect-dynamodb')({session: expressSession});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 app.set('view engine', 'ejs');
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -55,57 +53,11 @@ passportDb(passport);
 var flash = require('connect-flash');
 app.use(flash());
 
-//app.use('/', routes);
-// app.use('/home', homepage);
-// app.use('/faq', faq);
-// app.use('/users', users);
-// app.use('/auth', auth);
-// app.use('/getauth', getauth);
-// app.use('/getaction', getaction);
-// app.use('/getlight', getlight);
-// app.use('/temp', temp);
-// app.use('/reqlink', reqlink);
-// app.use('/verifylink', verifylink);
-// app.use('/logout', logout);
-// app.use('/', login);
-// app.use('/redirect', login);
-
-
-
 var alexa_app = require('./apps/printer');
 var home_app = require('./apps/home');
 home_app.express(app, "/alexa/");
 alexa_app.express(app, "/alexa/");
-//console.log(alexa_app.schema());
-//console.log(alexa_app.utterances());
 
-/*
-app.use (req, res, next) ->
-  if not req.headers.signaturecertchainurl
-    return next()
-  # mark the request body as already having been parsed so it's ignored by
-  # other body parser middlewares
-  req._body = true
-
-  req.rawBody = ''
-  req.on 'data', (data) ->
-    req.rawBody += data
-  req.on 'end', ->
-    try
-      req.body = JSON.parse req.rawBody
-    catch er 
-      req.body = {}
-
-    cert_url  = req.headers.signaturecertchainurl
-    signature = req.headers.signature
-    requestBody = req.rawBody
-    verifier cert_url, signature, requestBody, (er) ->
-      if er 
-        console.error 'error validating the alexa cert:', er
-        res.status(401).json { status: 'failure', reason: er }
-      else
-        next()
-*/
 app.use(require('./controllers'));
 
 // catch 404 and forward to error handler

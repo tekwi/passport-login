f5601396095e9547980e07e22a1e72f458807ba1
Sam Titus <sam.titus1@hp.com>
2016-09-16 16:05:11 -0700

    added flash messages for users admin

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -4,7 +4,7 @@ var express = require('express')
   , acl  = require('../lib/acl');
 
 router.get('/', acl, function(req, res) {
-    res.render('admin/index', {  redirect_url: null
+    res.render('admin/index', {redirect_url: null
     						   , redirect_message : null 
     						   , alexa_oauth : "https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.4f40bfef768c4087ab37f25ead297561&scope=alexa%3Aall&scope_data=%7B%22alexa%3Aall%22%3A%7B%22productID%22%3A%22macedon%22,%22productInstanceAttributes%22%3A%7B%22deviceSerialNumber%22%3A%20%22TH65A2Y124%22%7D%7D%7D&response_type=code&redirect_uri=https%3A%2F%2F"+ req.headers.host +"/auth"
     						   , info : req.session.login_message
@@ -14,6 +14,7 @@ router.get('/', acl, function(req, res) {
 })
 
 router.get('/redirect', acl, function(req, res) {
+  
     // res.render('admin/index')
 	res.render('admin/index', { seconds: 5 
 								, redirect_url : "https://www.amazon.com/ap/oa?client_id=amzn1.application-oa2-client.4f40bfef768c4087ab37f25ead297561&scope=alexa%3Aall&scope_data=%7B%22alexa%3Aall%22%3A%7B%22productID%22%3A%22macedon%22,%22productInstanceAttributes%22%3A%7B%22deviceSerialNumber%22%3A%20%22TH65A2Y124%22%7D%7D%7D&response_type=code&redirect_uri=https%3A%2F%2F" + req.headers.host + "/auth"
@@ -40,24 +41,31 @@ router.get('/charts', function(req, res) {
 // Handle get req to /users  
 router.get('/users', acl, function(req, res) {
    Users.getAll(function (err, user) {
-    res.render('admin/users',{users: user});
+    res.render('admin/users',{users: user 
+                              , info: req.flash('info')
+                              , warning: req.flash('warning')
+                              , danger: req.flash('danger')
+                             } 
+    );
   })
 });
 
 // Handle get req to /users  
 router.post('/users', acl, function(req, res) {
 
-  if(req.body.action == "disable-user") {   
+  if (req.body.action == "disable-user") {   
     var input = {"email" : req.body.key};
     var updates = {"status" : 2};
     Users.update(input, updates,function (err) {
+      req.flash('info', "Disable successful");
       res.json("Disable Success");
     }); 
   }
-  if(req.body.action == "reset-user") {   
+  if (req.body.action == "reset-user") {   
     var input = {"email" : req.body.key};
     var updates = {"status" : 3};
     Users.update(input, updates,function (err) {
+      req.flash('info', "Reset successful");
       res.json("Reset Success");
     });
   }
@@ -67,12 +75,15 @@ router.post('/users', acl, function(req, res) {
 // Handle get req to /users  
 router.delete('/users', acl, function(req, res) {
     var input = {"email" : req.body.key};
-    if(req.body.key != req.user.email){
+    if (req.body.key != req.user.email) {
       Users.delete(input,function (err) {
+        req.flash('info', "Delete successful");
         res.json("Delete Success");
       });
     }else{
+      req.flash('warning', "Self Delete not allowed");
       res.json("Self Delete not allowed");
+      
     }
 });
 
